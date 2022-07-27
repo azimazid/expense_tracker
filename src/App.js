f0a413eeabe6faf6083795234160d3c3693448e9
Azim Azidin <azimazid@gmail.com>
2022-07-28 06:44:37 +0800

    Pausing this project

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Details from "./components/Details/Details.jsx";
+import Main from "./components/Main/Main"
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <Grid
+        className={classes.grid}
         container
         spacing={0}
         alignItems="center"
@@ -14,13 +19,13 @@ const App = () => {
         style={{ height: "100vh" }}
       >
         <Grid item xs={12} sm={4}>
-            <Details />
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
         </Grid>
         <Grid item xs={12} sm={4}>
-            Main
-        </Grid>
-        <Grid item xs={12} sm={4}>
-            <Details />
+          <Details title="Expense" />
         </Grid>
       </Grid>
     </div>
